6753edbed08d7346c31b81ee1ed440cd27310c70
waternoise <135308451+waternoise@users.noreply.github.com>
2024-11-14 12:50:13 +0800

    idk just let it be

--- main.rb
+++ main.rb
@@ -1,19 +1,8 @@
 # Declarations and initializations
 operands = []
 operators = []
-ORDER = {"+": 1, "-": 1, "x": 2, "/": 2}
+ORDER = {"+" => 1, "-" => 1, "x" => 2, "/" => 2}
 
-# To number tranlator
-def turn_num(*numbers)
-    numbers.each do |n|
-        # What do if n is an operator
-        if true
-            puts "this is an operator"
-        else
-            puts "this is an operand"
-        end
-    end
+ARGV.each do |n|
+    puts n.class
 end
-
-turn_num(ARGV)
-
