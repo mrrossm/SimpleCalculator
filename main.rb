0dce3d95bd8ed79f0e617525e490e69756ddc11f
waternoise <135308451+waternoise@users.noreply.github.com>
2024-11-14 12:58:18 +0800

    Dumb operand-operator Separator

--- main.rb
+++ main.rb
@@ -3,6 +3,15 @@ operands = []
 operators = []
 ORDER = {"+" => 1, "-" => 1, "x" => 2, "/" => 2}
 
-ARGV.each do |n|
-    puts n.class
+ARGV.each do |ops|
+    if ORDER.has_key?(ops)
+        # If ops is an operator
+        operators.push(ops)
+    else
+        # If ops is an operand
+        operands.push(ops)
+    end
 end
+
+puts "operators", *operators
+puts "operands", *operands
