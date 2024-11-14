a554e26f8cba08ff33471e7cebd32dc2fd0c0c5a
waternoise <135308451+waternoise@users.noreply.github.com>
2024-11-14 15:46:27 +0800

    do_math and peek functions added

--- main.rb
+++ main.rb
@@ -3,6 +3,25 @@ operands = []
 operators = []
 ORDER = {"+" => 1, "-" => 1, "x" => 2, "/" => 2}
 
+#
+def do_math(operator, num_01, num_02)
+    case operator
+    when "+"
+        num_01 + num_02
+    when "-"
+        num_01 - num_02
+    when "x"
+        num_01 * num_02
+    when "/"
+        num_01 / num_02
+    end
+end
+
+#
+def peek(stack)
+    return stack[-1]
+end
+
 ARGV.each do |ops|
     if ORDER.has_key?(ops)
         # If ops is an operator
